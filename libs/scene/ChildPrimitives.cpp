2cfd165fa6a32c42ec6106bdd3d4360c02a7dca1
Klaus Silveira <contact@klaussilveira.com>
2026-03-25 06:00:37 -0400

    Fixed light entity child primitives being misplaced in Quake 4.

--- libs/scene/ChildPrimitives.cpp
+++ libs/scene/ChildPrimitives.cpp
@@ -3,12 +3,61 @@
 #include "ibrush.h"
 #include "ientity.h"
 #include "igroupnode.h"
+#include "ilightnode.h"
+#include "ipatch.h"
+#include "Translatable.h"
 
 #include "scene/EntityNode.h"
 #include "registry/registry.h"
+#include "string/convert.h"
 
 namespace scene
 {
+
+namespace
+{
+
+class ChildPrimitiveTranslator : public scene::NodeVisitor
+{
+	Vector3 _translation;
+public:
+	ChildPrimitiveTranslator(const Vector3& translation) :
+		_translation(translation)
+	{}
+
+	bool pre(const scene::INodePtr& node) override
+	{
+		Translatable* t = dynamic_cast<Translatable*>(node.get());
+		if (t)
+		{
+			t->translate(_translation);
+			return true;
+		}
+
+		IPatch* patch = Node_getIPatch(node);
+		if (patch)
+		{
+			for (std::size_t row = 0; row < patch->getHeight(); ++row)
+			{
+				for (std::size_t col = 0; col < patch->getWidth(); ++col)
+				{
+					patch->ctrlAt(row, col).vertex += _translation;
+				}
+			}
+			patch->controlPointsChanged();
+		}
+
+		return true;
+	}
+};
+
+void translateChildren(const scene::INodePtr& node, const Vector3& translation)
+{
+	ChildPrimitiveTranslator translator(translation);
+	node->traverseChildren(translator);
+}
+
+}
 
 // Local helper to add origins
 class OriginAdder :
@@ -31,6 +80,14 @@ public:
 			{
 				groupNode->addOriginToChildren();
 				// Don't traverse the children
+				return false;
+			}
+
+			if (Node_getLightNode(node) && !entity->isWorldspawn())
+			{
+				Vector3 origin = string::convert<Vector3>(
+					entity->getKeyValue("origin"));
+				translateChildren(node, -origin);
 				return false;
 			}
 		}
@@ -58,6 +115,14 @@ public:
 			{
 				groupNode->removeOriginFromChildren();
 				// Don't traverse the children
+				return false;
+			}
+
+			if (Node_getLightNode(node) && !entity->isWorldspawn())
+			{
+				Vector3 origin = string::convert<Vector3>(
+					entity->getKeyValue("origin"));
+				translateChildren(node, origin);
 				return false;
 			}
 		}
